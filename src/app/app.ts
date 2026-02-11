89091c772ae3638f65a4f2f6dc1b5d66e621e40a
Jonathan Cantu <jonathant.Cantu@gmail.com>
2026-02-11 04:37:13 -0600

    Deleted folder and created new project

--- src/app/app.ts
+++ src/app/app.ts
@@ -8,5 +8,5 @@ import { RouterOutlet } from '@angular/router';
   styleUrl: './app.css'
 })
 export class App {
-  protected readonly title = signal('AngularToDo');
+  protected readonly title = signal('AngularToDoApp');
 }
